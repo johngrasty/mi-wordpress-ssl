fe47f8024ceca3b4e71d2243ac23af373137ed72
John Grasty <john@needfaith.org>
2015-02-28 20:52:35 +0100

    Added directory needed for salt keys to be added.

--- copy/var/zoneinit/includes/31-salt.sh
+++ copy/var/zoneinit/includes/31-salt.sh
@@ -3,8 +3,12 @@ echo "master: "$(mdata-get salt-master) >> /opt/salt/etc/minion
 echo "id: "$(mdata-get salt-id) >> /opt/salt/etc/minion 
 
 #mkdir /data/salt/pki/minion
+mkdir /opt/salt/etc/pki/minion
+chmod 700 /opt/salt/etc/pki/minion
 mdata-get salt-pem > /opt/salt/etc/pki/minion/minion.pem
+chmod 400 /opt/salt/etc/pki/minion/minion.pem
 mdata-get salt-pub > /opt/salt/etc/pki/minion/minion.pub
+chmod 644 /opt/salt/etc/pki/minion/minion.pub
 /usr/sbin/svcadm enable salt-minion
 
 salt-call state-highstate
