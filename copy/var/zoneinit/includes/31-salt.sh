a20aba4a78b141af4d6e5efe76f707e591c798a1
John Grasty <john@needfaith.org>
2015-02-28 23:12:57 +0100

    Fixed type and tweaked salt install.

--- copy/var/zoneinit/includes/31-salt.sh
+++ copy/var/zoneinit/includes/31-salt.sh
@@ -3,7 +3,7 @@ echo "master: "$(mdata-get salt-master) >> /opt/salt/etc/minion
 echo "id: "$(mdata-get salt-id) >> /opt/salt/etc/minion 
 
 #mkdir /data/salt/pki/minion
-mkdir /opt/salt/etc/pki/minion
+mkdir -p /opt/salt/etc/pki/minion
 chmod 700 /opt/salt/etc/pki/minion
 mdata-get salt-pem > /opt/salt/etc/pki/minion/minion.pem
 chmod 400 /opt/salt/etc/pki/minion/minion.pem
