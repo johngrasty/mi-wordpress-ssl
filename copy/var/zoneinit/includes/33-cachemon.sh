beb6bfcbb335da19eaac008198d17adadb83d7d3
John Grasty <john@needfaith.org>
2015-03-01 02:25:47 +0100

    Removed cachemon

--- copy/var/zoneinit/includes/33-cachemon.sh
+++
@@ -1,6 +0,0 @@
-log "Creating Cache Web Monitoring"
-cd /data/www/wordpress
-mkdir opcache
-cd opcache
-curl -kLO  https://raw.github.com/rlerdorf/opcache-status/master/opcache.php
-chown -R www:www /data/www/wordpress
